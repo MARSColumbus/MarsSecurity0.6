2869bbd3c373c73f898907238a7c28768f386aff
bulabu <soundarajan3@gmail.com>
2024-12-08 13:08:44 -0500

    Delete build files

--- build/config.groovy
+++
@@ -1,8 +0,0 @@
-
-withConfig(configuration) {
-    inline(phase: 'CONVERSION') { source, context, classNode ->
-        classNode.putNodeMetaData('projectVersion', '8.0')
-        classNode.putNodeMetaData('projectName', 'MarsSecurity')
-        classNode.putNodeMetaData('isPlugin', 'true')
-    }
-}
